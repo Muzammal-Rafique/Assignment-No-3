60bdf184cd4ee3926bf291b097f84a32054e054c
Muzammal-Rafique <muzammalrafique26@gmail.com>
2023-12-17 20:41:08 +0500

    adjust the size of the card

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ const BlogList = () => {
     <div className="flex flex-wrap justify-center">
       {blogs.map(blog => (
         <Link key={blog.id} href={`/${blog.id}`}>    
-            <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-              <img className="w-full" src={blog.image} alt={blog.title} />
+            <div className="rounded w-80 h-80 overflow-hidden shadow-lg m-4">
+              <img className="w-full h-52" src={blog.image} alt={blog.title} />
               <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2 text-black">{blog.title}</div>
               </div>
